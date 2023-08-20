1d8db0c4457096ad1641bf5458eb30d6a65907ea
Nicolas DENIS <me@scoopandrun.ovh>
2023-08-20 13:41:03 +0200

    Fix firebase.ts
    Connexion aux émulateurs uniquement en local.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -26,21 +26,19 @@ const firebaseConfig: FirebaseOptions = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 const firestore = initializeFirestore(app, {
   localCache: persistentLocalCache(/*settings*/ {}),
 });
-connectFirestoreEmulator(firestore, "127.0.0.1", 8080);
-
 const auth = getAuth(app);
-connectAuthEmulator(auth, "http://127.0.0.1:9099");
-
 const functions = getFunctions(app);
-connectFunctionsEmulator(functions, "127.0.0.1", 5001);
-
 const storage = getStorage(app);
-connectStorageEmulator(storage, "127.0.0.1", 9199);
-
 const analytics = getAnalytics(app);
+
+if (["localhost", "127.0.0.1"].includes(window.location.hostname)) {
+  connectFirestoreEmulator(firestore, "127.0.0.1", 8080);
+  connectAuthEmulator(auth, "http://127.0.0.1:9099");
+  connectFunctionsEmulator(functions, "127.0.0.1", 5001);
+  connectStorageEmulator(storage, "127.0.0.1", 9199);
+}
 
 export { app, firestore, auth, analytics };
